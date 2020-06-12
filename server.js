52af7034c01b16dac3d9c5ae04394f0621b1359a
unknown <asotjrs@gmail.com>
2020-06-13 00:42:39 +0200

    adding port

--- server.js
+++ server.js
@@ -60,9 +60,9 @@ app.post('/imageUrl',image.handleInputImage);
     // res == true
 //});
 
-PORT=process.env.PORT;
-app.listen(PORT,()=>{
 
-console.log('app is running on port '+PORT);
+app.listen(process.env.PORT || 3000,()=>{
+
+console.log('app is running on port '+process.env.PORT);
 
 });
