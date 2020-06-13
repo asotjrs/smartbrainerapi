91c61a3a8b57bbc0581a8d7613c6e2fdc28db8bc
unknown <asotjrs@gmail.com>
2020-06-13 02:31:16 +0200

    adding pg

--- server.js
+++ server.js
@@ -10,35 +10,14 @@ const db = require('knex')(
     {
         client:'pg',
         connection:{
-            host:'127.0.0.1',
-            user:'postgres',
-            password:'Aso020202',
-            database:'smart-brain'
+            connectionString: process.env.DATABASE_URL,
+            ssl:true,
         }
     }
 );
 const app=express();
 app.use(bodyPaser.json());
 app.use(cors());
-const database={ users:[
-        {
-            id:'123',
-            name:'kk',
-            email:'kk@gmail.com',
-            password:'kk',
-            entries:0,
-            joined: new Date()
-        },
-{
-    id:'124',
-        name:'amine',
-    email:'amine@gmail.com',
-    password:'amine02',
-    entries:0,
-    joined: new Date()
-}]
-
-};
 
 app.get('/',(req,res)=>{
 
