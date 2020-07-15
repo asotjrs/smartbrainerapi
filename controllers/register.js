aaed7f535eb1808c17463f249ebf20f735a9a1fa
unknown <asotjrs@gmail.com>
2020-07-15 15:56:07 +0200

    adding more

--- controllers/register.js
+++ controllers/register.js
@@ -6,30 +6,24 @@ const handleRegister=(db,bcrypt)=>(req,res)=>{
     if (!email || !name || !password)
         return res.status(400).json("can't register with blank fields");
 
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
+    bcrypt.hash(password, 10, function(err, hash) {
+
+        db.transaction(trx => {
+            trx.insert({hash:hash, email:email}).into('login').returning('email').
+            then(returnedEmail=>{
+                return trx('users').returning('*')
                     .insert({
-                        email: loginEmail[0],
-                        name: name,
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch(err => res.status(400).json('unable to register'))
+                        name:name,
+                        email:returnedEmail[0],
+                        joined:new Date()
+                    }).then(users=>{res.json(users[0])} );
+            }).then(trx.commit).catch(trx.rollback)
+
+        }).catch(err=>res.status(400).json("unable to register"));
+
+    });
+
+
 };
 module.exports={
 
